2aec0684e3a396e37067910ce4f60902d72a5729
Jack Franklin <jack@jackfranklin.net>
2017-02-13 09:03:53 +0000

    Final workshop bits

--- testing-app/src/ListOfPeople.test.js
+++ testing-app/src/ListOfPeople.test.js
@@ -1,9 +1,19 @@
 import { mount } from 'enzyme'
 import ListOfPeople from './ListOfPeople'
 import React from 'react'
+import renderer from 'react-test-renderer'
 
 test('it renders the heading', () => {
   const wrapper = mount(<ListOfPeople people={[]} />)
   expect(wrapper.find('h2').text()).toEqual(`List of Jack's Friends`)
+})
+
+test('it matches the snapshot', () => {
+  const tree = renderer.create(
+    <ListOfPeople people={[
+      { id: 1, name: 'Jack', colour: 'blue' },
+    ]} />
+  ).toJSON()
+  expect(tree).toMatchSnapshot()
 })
 
